8991759428473c4749dfe4ee86ab9becee715d83
Aldwin <aldwin828@gmail.com>
2023-07-06 13:57:16 -0400

    Added function that slect random quote

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let apiQuotes = [];
 
+// Show random quote
+function randomQuote() {
+    // select random quote from the api
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
 // Get quotes from api
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes[12]);
+        randomQuote();
     } catch (error) {
         // Catch error here
     }
