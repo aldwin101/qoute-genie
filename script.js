254d1031051c95f5d426166306af689652e64ff3
Aldwin <aldwin828@gmail.com>
2023-07-07 13:55:48 -0400

    In progress: Displaying dynamic quote and author

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 let apiQuotes = [];
 
 // Show random quote
 function randomQuote() {
     // select random quote from the api
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
     console.log(quote);
 }
 
