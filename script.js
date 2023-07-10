2fd1a5f2e0b9b6cfcf6e35413a121ac1bbd7c5d2
Aldwin <aldwin828@gmail.com>
2023-07-10 13:54:31 -0400

    Added loading animation

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 let apiQuotes = [];
+
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading after data has been loaded into DOM
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 
 // Show random quote
 function randomQuote() {
+    loading();
     // select random quote from the api
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if author field is blank and replace it with unknown
@@ -22,12 +37,14 @@ function randomQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
-    
+    // Set quote and hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get quotes from api
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
